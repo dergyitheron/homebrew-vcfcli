4f5200bc76c22d0151f12b50e120dec0948a877d
David Vrtěl <57442022+dergyitheron@users.noreply.github.com>
2026-03-12 19:19:37 +0100

    feat: linux-amd64 and linux-arm64 versions added

--- vcf-cli.rb
+++ vcf-cli.rb
@@ -9,14 +9,19 @@ class VcfCli < Formula
   checksums = {
     "darwin-amd64" => "2d677c32ffeae550f7551bd54b175be6df4cabc79e969acfed94895ff22e3073",
     "darwin-arm64" => "e3cb3fa6956d2f4ab60dd555191d05f90c0170a47d57e3a50f742570f9e673a2",
+    "linux-amd64"  => "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+    "linux-arm64"  => "281aa1dd040c20ca0b3415810d4b0cda5d0b0ea1ee5fa2e0bf37d2709396576b",
   }
 
   $arch = "arm64"
   on_intel do
     $arch = "amd64"
   end
-
+  
   $os = "darwin"
+  on_linux do
+    $os = "linux"
+  end
 
   url "https://packages.broadcom.com/artifactory/vcf-distro/vcf-cli/#{$os}/#{$arch}/v#{version}/vcf-cli.tar.gz"
   sha256 checksums["#{$os}-#{$arch}"]
