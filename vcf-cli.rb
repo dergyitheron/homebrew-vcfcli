d669385b1b511cd39241a9258f8e5a8ebfac1c3d
David Vrtěl <57442022+dergyitheron@users.noreply.github.com>
2026-03-12 20:13:49 +0100

    chore: formatting

--- vcf-cli.rb
+++ vcf-cli.rb
@@ -7,17 +7,17 @@ class VcfCli < Formula
   version "9.0.2"
 
   checksums = {
-     "darwin-amd64" => "2d677c32ffeae550f7551bd54b175be6df4cabc79e969acfed94895ff22e3073",
-     "darwin-arm64" => "e3cb3fa6956d2f4ab60dd555191d05f90c0170a47d57e3a50f742570f9e673a2",
-     "linux-amd64"  => "a4595fe7363b290fe206255b4c2d7728b95c27a1ed5606549063161c85627c47",
-     "linux-arm64"  => "281aa1dd040c20ca0b3415810d4b0cda5d0b0ea1ee5fa2e0bf37d2709396576b",
-   }
+    "darwin-amd64" => "2d677c32ffeae550f7551bd54b175be6df4cabc79e969acfed94895ff22e3073",
+    "darwin-arm64" => "e3cb3fa6956d2f4ab60dd555191d05f90c0170a47d57e3a50f742570f9e673a2",
+    "linux-amd64"  => "a4595fe7363b290fe206255b4c2d7728b95c27a1ed5606549063161c85627c47",
+    "linux-arm64"  => "281aa1dd040c20ca0b3415810d4b0cda5d0b0ea1ee5fa2e0bf37d2709396576b",
+  }
   
   $arch = "arm64"
   on_intel do
     $arch = "amd64"
   end
-  
+
   $os = "darwin"
   on_linux do
     $os = "linux"
